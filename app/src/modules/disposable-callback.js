e5483c574c5373a72f395c8b9c9512da6f4066e9
Max Shishkin <shishkin.max@gmail.com>
2012-11-21 23:53:33 +0400

    Small refactor to callback module

--- app/src/modules/disposable-callback.js
+++ app/src/modules/disposable-callback.js
@@ -1,15 +1,15 @@
-(function ($, Disposable, undefined) {
+(function (Disposable, undefined) {
   /**
    * A callback function wrapper
    * Returns a new function that can be disposed
    */
-  $.Disposable.prototype.callback = function (fn, ctx) {
+  Disposable.prototype.callback = function (fn, ctx) {
     var that = this;
 
     return !this._disposed && function () {
-      if (that._disposed) return;
-      return fn.apply(ctx || this, arguments);
+      return that._disposed ?
+        undefined : fn.apply(ctx || this, arguments);
     }
   };
 
-}(jQuery, jQuery.Disposable));
+}(jQuery.Disposable));
