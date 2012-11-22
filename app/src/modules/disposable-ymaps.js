03792d178b5a9722acc9535a22b9f0ca5f00afb6
Max Shishkin <shishkin.max@gmail.com>
2012-11-22 11:36:03 +0400

    Ymaps Class stores IEventManager object
    I.e. it stores elem.events rather than elem itself.

--- app/src/modules/disposable-ymaps.js
+++ app/src/modules/disposable-ymaps.js
@@ -2,8 +2,8 @@
   /**
    * Creates a ymaps object wrapper
    */
-  var Class = function (elem, disposable) {
-    this._elem = elem;
+  var Class = function (events, disposable) {
+    this._events = events;
     this._disposable = disposable;
   };
 
@@ -12,14 +12,12 @@
    * Returns itself for further chaining
    */
   Class.prototype.on = function () {
-    var events = this._elem.events;
-
     this._disposable._ymaps.push({
-      context: events,
+      context: this._events,
       args: arguments
     });
 
-    events.add.apply(events, arguments);
+    this._events.add.apply(this._events, arguments);
 
     return this;
   };
@@ -29,7 +27,7 @@
    * Returns interface for attaching events to the wrapped object
    */
   Disposable.prototype.ymaps = function (elem) {
-    return !this._disposed && new Class(elem, this);
+    return !this._disposed && new Class(elem.events, this);
   };
 
   /**
